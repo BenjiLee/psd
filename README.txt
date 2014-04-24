a845d21639bd0fec4a768d17f542cb5089cf04f7
christopher lee <christopherlee2012@gmail.com>
2014-04-24 16:34:05 -0400

    put gpio.c in folder

---
+++ README.txt
@@ -0,0 +1,9 @@
+BitcoinTicker
+=============
+
+Personal Scanning Device
+
+Christopher Lee
+4/21/2014
+christopherlee2012@gmail.com
+
---
+++ gpio.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
+void scanButton (int button)
+{
+  int once;
+  if (digitalRead (button) == HIGH)
+        once = 0;
+  while (digitalRead (button) == LOW)   // Low is pushed
+    if (once ==0) {
+        printf("Button: %d\n", button);
+        switch(button)
+        {
+            case 1:
+            system ("sh ~/resetwifi.sh");
+            break;
+            case 2:
+            system ("sudo python ~/psd/sandbox.py");
+            break;
+            case 3:
+            printf("janet smells\n");
+        }
+        once = 1;
+    }
+}
+
+int main (void)
+{
+  int i ;
+  wiringPiSetup () ;
+
+  for (i = 1 ; i < 4 ; ++i)  {
+    pinMode         (i, INPUT) ;
+    pullUpDnControl (i, PUD_UP) ;
+    printf ("pin set: %d\n", i) ;
+  }
+  for (;;)  {
+    for (i = 1 ; i < 4 ; ++i)
+      scanButton (i) ;
+    delay (1) ;
+  }
+}
