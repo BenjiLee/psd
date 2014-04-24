7d8172c9cc1006b06c4fcbfc0ee5516832c24fbb
christopher lee <christopherlee2012@gmail.com>
2014-04-24 17:41:33 -0400

    fixed pathing for keyboard image

--- wifi.py
+++ wifi.py
@@ -32,8 +32,10 @@ focus = "ssid"
 back = "back"
 space = ""
 shift = "shift"
-#keyboard = pygame.image.load("/home/pi/psd/keyboard.png").convert()
-keyboard = pygame.image.load("keyboard.png").convert()
+if device == "raspberrypi":
+    keyboard = pygame.image.load("/home/pi/psd/keyboard.png").convert()
+else:
+    keyboard = pygame.image.load("keyboard.png").convert()
 alphabet = [['Q','W','E','R','T','Y','U','I','O','P'],
             ['A','S','D','F','G','H','J','K','L',back],
             ['Z','X','C','V','B','N','M',',','.',back],
